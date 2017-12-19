509aca26000036d152ae0f1a5b8a2c50a8db4317
WRAH <contact@wrahllc.com>
2017-12-19 12:13:10 +0100

    Production Rubric ID

--- www/src/Wrah/Swabbie/MainController.php
+++ www/src/Wrah/Swabbie/MainController.php
@@ -34,7 +34,7 @@ class MainController implements ControllerProviderInterface
   /*
   static public function articleAction(Application $application, $gmcHeader, $articleSlug)
   {
-    $mc2mRubricId = "2007366";
+    $mc2mRubricId = "2007421";
     if(!$gmcHeader)
     {
         $header = file_get_contents("http://www.magicmaman.com/chunk/top/". $mc2mRubricId);
@@ -73,7 +73,7 @@ class MainController implements ControllerProviderInterface
   
   static public function indexAction(Application $application, $gmcHeader)
   {
-    $mc2mRubricId = "2007366";
+    $mc2mRubricId = "2007421";
     if(!$gmcHeader)
     {
         $header = file_get_contents("http://www.magicmaman.com/chunk/top/". $mc2mRubricId);
